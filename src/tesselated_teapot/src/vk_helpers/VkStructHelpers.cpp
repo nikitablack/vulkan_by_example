1d8669366c3d05f1cd9720878c564d4805eaae88
nc <nikita.cherniy@ibeo-as.com>
2018-03-21 11:30:54 +0100

    Fixed naming

--- src/tesselated_teapot/src/vk_helpers/VkStructHelpers.cpp
+++ src/tesselated_teapot/src/vk_helpers/VkStructHelpers.cpp
@@ -8,60 +8,60 @@ namespace vk_helpers
 	VkInstanceCreateInfo get_instance_create_info(vector<char const *> const * const extensions = nullptr,
 	                                              vector<char const *> const * const layers = nullptr)
 	{
-		VkInstanceCreateInfo vkInstCreateInfo{};
-		vkInstCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		vkInstCreateInfo.pNext = nullptr;
-		vkInstCreateInfo.flags = 0;
-		vkInstCreateInfo.pApplicationInfo = nullptr;
-		vkInstCreateInfo.enabledLayerCount = (layers != nullptr) ? static_cast<uint32_t>(layers->size()) : 0;
-		vkInstCreateInfo.ppEnabledLayerNames = (layers != nullptr) ? layers->data() : nullptr;
-		vkInstCreateInfo.enabledExtensionCount = (extensions != nullptr) ? static_cast<uint32_t>(extensions->size()) : 0;
-		vkInstCreateInfo.ppEnabledExtensionNames = (extensions != nullptr) ? extensions->data() : nullptr;
+		VkInstanceCreateInfo info{};
+		info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		info.pNext = nullptr;
+		info.flags = 0;
+		info.pApplicationInfo = nullptr;
+		info.enabledLayerCount = (layers != nullptr) ? static_cast<uint32_t>(layers->size()) : 0;
+		info.ppEnabledLayerNames = (layers != nullptr) ? layers->data() : nullptr;
+		info.enabledExtensionCount = (extensions != nullptr) ? static_cast<uint32_t>(extensions->size()) : 0;
+		info.ppEnabledExtensionNames = (extensions != nullptr) ? extensions->data() : nullptr;
 		
-		return vkInstCreateInfo;
+		return info;
 	}
 	
 	VkDeviceQueueCreateInfo get_device_queue_create_info(uint32_t const queueFamilyIndex, vector<float> const * const queuePriorities)
 	{
-		VkDeviceQueueCreateInfo queueCreateInfo{};
-		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-		queueCreateInfo.pNext = nullptr;
-		queueCreateInfo.flags = 0;
-		queueCreateInfo.queueFamilyIndex = queueFamilyIndex;
-		queueCreateInfo.queueCount = (queuePriorities != nullptr) ? static_cast<uint32_t>(queuePriorities->size()) : 0;
-		queueCreateInfo.pQueuePriorities = (queuePriorities != nullptr) ? queuePriorities->data() : nullptr;
+		VkDeviceQueueCreateInfo info{};
+		info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		info.pNext = nullptr;
+		info.flags = 0;
+		info.queueFamilyIndex = queueFamilyIndex;
+		info.queueCount = (queuePriorities != nullptr) ? static_cast<uint32_t>(queuePriorities->size()) : 0;
+		info.pQueuePriorities = (queuePriorities != nullptr) ? queuePriorities->data() : nullptr;
 		
-		return queueCreateInfo;
+		return info;
 	}
 	
 	VkDeviceCreateInfo get_device_create_info(vector<VkDeviceQueueCreateInfo> const * const queueCreateInfos,
 	                                          vector<char const *> const * const deviceExtensions,
 	                                          VkPhysicalDeviceFeatures const * const deviceFeatures)
 	{
-		VkDeviceCreateInfo createInfo{};
-		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-		createInfo.pNext = nullptr;
-		createInfo.flags = 0;
-		createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos->size());
-		createInfo.pQueueCreateInfos = queueCreateInfos->data();
-		createInfo.enabledLayerCount = 0;
-		createInfo.ppEnabledLayerNames = nullptr;
-		createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions->size());
-		createInfo.ppEnabledExtensionNames = deviceExtensions->data();
-		createInfo.pEnabledFeatures = deviceFeatures;
+		VkDeviceCreateInfo info{};
+		info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		info.pNext = nullptr;
+		info.flags = 0;
+		info.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos->size());
+		info.pQueueCreateInfos = queueCreateInfos->data();
+		info.enabledLayerCount = 0;
+		info.ppEnabledLayerNames = nullptr;
+		info.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions->size());
+		info.ppEnabledExtensionNames = deviceExtensions->data();
+		info.pEnabledFeatures = deviceFeatures;
 		
-		return createInfo;
+		return info;
 	}
 	
 	VkShaderModuleCreateInfo get_shader_module_create_info(vector<char> const * const shaderCode)
 	{
-		VkShaderModuleCreateInfo createInfo{};
-		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-		createInfo.pNext = nullptr;
-		createInfo.flags = 0;
-		createInfo.codeSize = shaderCode->size();
-		createInfo.pCode = reinterpret_cast<uint32_t const *>(shaderCode->data());
+		VkShaderModuleCreateInfo info{};
+		info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		info.pNext = nullptr;
+		info.flags = 0;
+		info.codeSize = shaderCode->size();
+		info.pCode = reinterpret_cast<uint32_t const *>(shaderCode->data());
 		
-		return createInfo;
+		return info;
 	}
 }
