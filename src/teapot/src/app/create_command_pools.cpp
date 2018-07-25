5c9d8113dcd318651bd4fab274b11bb861db7897
nc <nikita.cherniy@ibeo-as.com>
2018-07-25 09:46:12 +0200

    Refactored command pools.

--- src/teapot/src/app/create_command_pools.cpp
+++ src/teapot/src/app/create_command_pools.cpp
@@ -10,29 +10,21 @@ MaybeAppData create_command_pools(AppData data)
 {
 	assert(data.device);
 	
-	helpers::MaybeCommandPool const mbRenderCommandPool{helpers::create_command_pool(data.device, data.graphicsFamilyQueueIndex)};
-	if (!mbRenderCommandPool)
-		return tl::make_unexpected(mbRenderCommandPool.error());
+	helpers::MaybeCommandPool const mbStaticCommandPool{helpers::create_command_pool(data.device, data.graphicsFamilyQueueIndex, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT)};
+	if (!mbStaticCommandPool)
+		return tl::make_unexpected(mbStaticCommandPool.error());
 	
-	data.renderCommandPool = *mbRenderCommandPool;
+	data.staticCommandPool = *mbStaticCommandPool;
 	
-	helpers::set_debug_utils_object_name(data.instance, data.device, VK_OBJECT_TYPE_COMMAND_POOL, reinterpret_cast<uint64_t>(data.renderCommandPool), "render command pool");
+	helpers::set_debug_utils_object_name(data.instance, data.device, VK_OBJECT_TYPE_COMMAND_POOL, reinterpret_cast<uint64_t>(data.staticCommandPool), "static command pool");
 	
-	helpers::MaybeCommandPool const mbPushConstantsCommandPool{helpers::create_command_pool(data.device, data.graphicsFamilyQueueIndex, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT)};
-	if (!mbPushConstantsCommandPool)
-		return tl::make_unexpected(mbPushConstantsCommandPool.error());
+	helpers::MaybeCommandPool const mbDynamicCommandPool{helpers::create_command_pool(data.device, data.graphicsFamilyQueueIndex, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT)};
+	if (!mbDynamicCommandPool)
+		return tl::make_unexpected(mbDynamicCommandPool.error());
 	
-	data.pushConstantsCommandPool = *mbPushConstantsCommandPool;
+	data.dynamicCommandPool = *mbDynamicCommandPool;
 	
-	helpers::set_debug_utils_object_name(data.instance, data.device, VK_OBJECT_TYPE_COMMAND_POOL, reinterpret_cast<uint64_t>(data.pushConstantsCommandPool), "push constant command pool");
-	
-	helpers::MaybeCommandPool const mbUpdateMatricesCommandPool{helpers::create_command_pool(data.device, data.graphicsFamilyQueueIndex, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT)};
-	if (!mbUpdateMatricesCommandPool)
-		return tl::make_unexpected(mbUpdateMatricesCommandPool.error());
-	
-	data.updateMatricesCommandPool = *mbUpdateMatricesCommandPool;
-	
-	helpers::set_debug_utils_object_name(data.instance, data.device, VK_OBJECT_TYPE_COMMAND_POOL, reinterpret_cast<uint64_t>(data.updateMatricesCommandPool), "update matrices command pool");
+	helpers::set_debug_utils_object_name(data.instance, data.device, VK_OBJECT_TYPE_COMMAND_POOL, reinterpret_cast<uint64_t>(data.dynamicCommandPool), "dynamic command pool");
 	
 	return data;
 }
